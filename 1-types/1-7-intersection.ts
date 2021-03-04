92151e379442d554923bcb33893b7d56a0d00d7a
kyw <[duddnr654@naver.com]>
2021-03-04 22:00:28 +0900

    2.12 Intersection 타입 ✨

--- 1-types/1-7-intersection.ts
+++ 1-types/1-7-intersection.ts
@@ -12,6 +12,9 @@
     work: () => void;
   };
 
+  
+  // Student와 Workder의 타입을 모두 접근할 수 있는 대신에
+  // 두 타입의 key값들을 다 인자로 넣어주어야 합니다.
   function internWork(person: Student & Worker) {
     console.log(person.name, person.empolyeeId, person.work());
   }
