05b19a9bb82220685dcc3c2a1a910e2e921b062c
kyw <duddnr654@naver.com>
2021-03-08 21:35:26 +0900

    3.7 로딩 상태 표시 💡

--- 2-types-projects-quiz/loading.ts
+++ 2-types-projects-quiz/loading.ts
@@ -20,6 +20,36 @@
 
   type ResourceLoadState = LoadingState | SuccessState | FailState;
 
+
+  // 내가 작성한 코드
+  // function printLoginState(state: ResourceLoadState, response?:) {
+  //   if(state.state === 'loading') {
+  //     console.log('👀 loading...')
+  //   } else if(state.state === 'success') {
+
+  //   }
+  // }
+
+  
+  // 엘리가 작성한 코드
+  function printLoginState(state: ResourceLoadState) {
+    switch(state.state) {
+      case 'loading':
+        console.log('👀 loading...');
+        break;
+      case 'success':
+        console.log(`😃 ${state.response.body}`);
+        break;
+      case 'fail':
+        console.log(`😱 ${state.reason}`);
+        break;
+      default:
+        throw new Error(`unknown state: ${state}`);
+      }
+  }
+  
+
+
   printLoginState({ state: 'loading' }); // 👀 loading...
   printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
   printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
