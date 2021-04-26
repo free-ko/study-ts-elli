3a8523616f6ec594ac4a73181a6d38778596fe45
kyw <duddnr654@naver.com>
2021-04-26 21:34:52 +0900

    14.19 아이템 삭제 기능 구현 하기 💡

--- motion/src/components/page/page.ts
+++ motion/src/components/page/page.ts
@@ -3,37 +3,48 @@ import { BaseComponent, Component } from "./../component.js";
 export interface Composable {
   addChild(child: Component): void;
 }
+type OnCloseListener = () => void;
 
 class PageItemComponent
   extends BaseComponent<HTMLElement>
   implements Composable {
-  constructor() {
-    super(` <li class="page-item">
-        <section class="page-item__body"></section>
-        <div class="page-item__controls">
-            <button class="clone">&times;</button>
-        </div>
-    </li>`);
-  }
+  private closeListener?: OnCloseListener;
 
+  constructor() {
+    super(`<li class="page-item">
+            <section class="page-item__body"></section>
+            <div class="page-item__controls">
+              <button class="close">&times;</button>
+            </div>
+          </li>`);
+    const closeBtn = this.element.querySelector(".close")! as HTMLButtonElement;
+    closeBtn.onclick = () => {
+      this.closeListener && this.closeListener();
+    };
+  }
   addChild(child: Component) {
     const container = this.element.querySelector(
       ".page-item__body"
     )! as HTMLElement;
     child.attachTo(container);
   }
+  setOnCloseListener(listener: OnCloseListener) {
+    this.closeListener = listener;
+  }
 }
-
-export class PageComponet
+export class PageComponent
   extends BaseComponent<HTMLUListElement>
   implements Composable {
   constructor() {
-    super('<ul class="page">This is PageComponent!</ul>');
+    super('<ul class="page"></ul>');
   }
 
   addChild(section: Component) {
     const item = new PageItemComponent();
     item.addChild(section);
     item.attachTo(this.element, "beforeend");
+    item.setOnCloseListener(() => {
+      item.removeFrom(this.element);
+    });
   }
 }
