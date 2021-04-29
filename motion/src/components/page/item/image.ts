7d61f31e8e643dab51e1bc9a804008de7813fa5e
kyw <duddnr654@naver.com>
2021-04-29 13:37:08 +0900

    14.28 마지막 마무리, 스타일링 ✨

--- motion/src/components/page/item/image.ts
+++ motion/src/components/page/item/image.ts
@@ -1,25 +1,21 @@
-import { BaseComponent } from './../../component.js';
-export class ImageComponent extends BaseComponent<HTMLElement>{
+import { BaseComponent } from "./../../component.js";
 
+export class ImageComponent extends BaseComponent<HTMLElement> {
+  constructor(title: string, url: string) {
+    super(`<section class="image">
+            <div class="image__holder"><img class="image__thumbnail"></div>
+            <h2 class="page-item__title image__title"></h2>
+          </section>`);
 
-    constructor(title: string, url: string) {
-        super(` 
-            <section class="image">
-                <div class="image__holder">
-                    <img class="image__thumbnail">
-                    <h2 class="image__title"></h2>
-                </div>
-            </section>
-        `)
+    const imageElement = this.element.querySelector(
+      ".image__thumbnail"
+    )! as HTMLImageElement;
+    imageElement.src = url;
+    imageElement.alt = title;
 
-        const imageElement = this.element.querySelector('.image__thumbnail')! as HTMLImageElement;
-        imageElement.src = url;
-        imageElement.alt = title;
-    
-        const titleElement = this.element.querySelector('.image__title')! as HTMLHeadingElement;
-        titleElement.textContent = title; 
-    }
+    const titleElement = this.element.querySelector(
+      ".image__title"
+    )! as HTMLParagraphElement;
+    titleElement.textContent = title;
+  }
 }
-
-
-
--- motion/src/components/page/item/todo.ts
+++ motion/src/components/page/item/todo.ts
@@ -1,17 +1,36 @@
-import { BaseComponent } from './../../component.js';
-
-export class TodoComponent extends BaseComponent<HTMLElement> {
-    constructor(title: string, todo: string) {
-        super(`
-        <section class="todo">
-            <h2 class="todo__title"></h2>
-            <input type="checkbox" class="todo__checkbox">
+import { BaseComponent } from "./../../component.js";
+export class VideoComponent extends BaseComponent<HTMLElement> {
+  constructor(title: string, url: string) {
+    super(`<section class="video">
+            <div class="video__player"><iframe class="video__iframe"></iframe></div>
+            <h3 class="page-item__title video__title"></h3>
         </section>`);
 
-        const titleElement = this.element.querySelector('.todo__title')! as HTMLHeadingElement;
-        titleElement.textContent = title;
+    const iframe = this.element.querySelector(
+      ".video__iframe"
+    )! as HTMLIFrameElement;
+    iframe.src = this.convertToEmbeddedURL(url);
 
-        const todoElement = this.element.querySelector('.todo__checkbox')! as HTMLInputElement;
-        todoElement.insertAdjacentText('afterend', todo);
+    const titleElement = this.element.querySelector(
+      ".video__title"
+    )! as HTMLHeadingElement;
+    titleElement.textContent = title;
+  }
+
+  // input
+  // https://www.youtube.com/watch?v=K3-jG52XwuQ
+  // https://youtu.be/K3-jG52XwuQ
+  // output
+  // https://www.youtube.com/embed/K3-jG52XwuQ
+  // 정규표현식 Regex
+  // https://regexr.com/5l6nr
+  private convertToEmbeddedURL(url: string): string {
+    const regExp = /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/;
+    const match = url.match(regExp);
+    const videoId = match ? match[1] || match[2] : undefined;
+    if (videoId) {
+      return `https://www.youtube.com/embed/${videoId}`;
     }
+    return url;
+  }
 }
